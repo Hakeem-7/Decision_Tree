cd4899b48d964ae94484b2a5d51cdd15805e5cf4
Hakeem-7 <52264207+Hakeem-7@users.noreply.github.com>
2020-03-18 20:05:19 -0500

    Implementation of C5.0 and the embedded boosting algorithm.

--- Raw Script.R
+++ Raw Script.R
@@ -1,4 +1,4 @@
-#Decision Tree Algo using the C5.0 algorithm by J. Ross Quinlanb (Industry Standard)
+#Decision Tree Algo using the C5.0 algorithm by J. Ross Quinlanb (Industry Standard) - Divide and Conquer
 
 curve(-x * log2(x) - (1 - x) * log2(1 - x),
     col = "darkred", xlab = "x", ylab = "Entropy", lwd = 3) #Illustration of entropy; 50-50 split results in maximum entropy
@@ -9,6 +9,13 @@ str(train)
 table(train$Label)  #summarizes the data in the feature
 ClassVariables = sapply(train, function(x) class(x)) #Class Variables
 ClassVariables
+
+Classes <- function(train){
+  Class_variables <- sapply(train, function(x) class(x)) #component of the function should be the adjustable parameter
+  return(Class_variables)
+}
+Classes(train_factor)  #rm(cv) - removed the previous function
+
 
 #train_sample <- sample(1000, 900) - Samples 900 from 1000 observations.
 install.packages("C50") #Decision tree algo that implement entropy criterion
@@ -33,8 +40,7 @@ table(test$Label)
 test_factor = test %>%
   mutate_at(vars(Label),
             funs(factor))
-cv = sapply(test_factor, function(y) class(y)) #confirmation test that the mutation worked
-cv
+Classes(test_factor)      #confirmation test that the mutation worked
 
 # Model performance evaluation
 model_pred <- predict(model, test_factor)
@@ -44,7 +50,14 @@ CrossTable(test_factor$Label, model_pred, prop.chisq = FALSE, prop.c = FALSE, pr
            dnn = c('actual', 'predicted')) 
 
 # Model performance improvement - Boosting the accuracy of decision trees
+#C5.0 algorithm improved upon the C4.5 algo through the adoption of adaptive boosting.
+model_boost <- C5.0(train_factor[-1], train_factor$Label, trials = 10) #Research suggested that "trials = 10" improves tree accuracy by 25%)
+model_boost
+summary(model_boost)
+model_boost_pred <- predict(model_boost, test_factor)
+CrossTable(test_factor$Label, model_boost_pred, prop.r = F, prop.c = F, prop.chisq = F,
+           dnn = c("actual", "predicted"))
 
+# Addressing false negatives - especially in sensitive cases like bank loans
 
-
-
+ 
